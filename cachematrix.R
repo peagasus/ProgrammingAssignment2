8bb365619cdd5ed99a948a8f6069bf1acc74e7da
peagasus <emin1907@gmail.com>
2015-09-27 01:30:28 +0200

    AssignmentCompleted
    Assignment is completed and the functions are found to be working.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file includes functions related to creation
+##     of matrices which can store (or cache) inverted 
+##     versions and calculation of this inversion
+##     considering this matrix class
 
-## Write a short comment describing this function
+## makeCacheMatrix funtion creates a matrix object that 
+##     can store inverted version of itself
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Set initial values
+  inv <- NULL
+  ifchanged <- TRUE
+  
+  # Create set/get functions
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+    ifchanged <<- TRUE    
+    # If new values entered, change ifchanged flag
+  }
+  get <- function() x
+  setinv <- function(invVal) inv <<- invVal
+  getinv <- function() inv
+  setifch <- function(ifchangedVal) ifchanged <<- ifchangedVal
+  getifch <- function() ifchanged
+  
+  # Return list of set/get functions
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv,
+       setifch = setifch,
+       getifch = getifch)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that can compute the inverse
+##     of a matrix, or return the inverse value of a matrix
+##     if it is stored in the input matrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  # Check if inverse exist and matrix didn't change
+  inv <- x$getinv()
+  ifchanged <- x$getifch()
+  if(!is.null(inv) && !ifchanged) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # Calculate the inverse of the matrix if it isn't cached
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  x$setifc(FALSE)   # Change the flag after calculation
+  
+  # Return inverse of the matrix
+  inv
 }
